cb682861b33e7580acda97a306a488c03871f83d
Jean Urena <juanpedro1519@gmail.com>
2017-09-18 23:49:50 -0400

    [JS-DP] - Cat Clicker Premium

--- Courses/JS_Design_Patterns/cat_clicker/assets/js/script.js
+++ Courses/JS_Design_Patterns/cat_clicker/assets/js/script.js
@@ -24,43 +24,52 @@ var cats = {
 	}
 }
 
-var cat_html = `
-	<div class="cat">
-		<h1>{{cat_name}}</h1>
-		<img src="{{cat_image}}">
-		<h2 id="clicker_count_{{cat_id}}">{{cat_click_count}}</h2>
-		<button class="clicker_button" id="cat_{{cat_id}}_button" data-cat_id={{cat_id}}>Vote for {{cat_name}}</button>
-	</div>
+// html template for the list item
+var cat_list_html = `
+	<a href="#" class="list-group-item cat-link" id="cat_{{cat_id}}_link" data-cat_id={{cat_id}}>{{cat_name}}</a>
 `;
 
-// this will be generated
-var cats_html = "";
-// console.log(cat_html.replace("{{cat_name}}", cats["1"]["cat_name"]));
+// html template for the info container
+var cat_html = `
+	<h2 class="text-center">{{cat_name}}</h2>
+	<img src="{{cat_image}}" class="img-responsive img-circle center-block">
+	<h2 id="clicker_count_{{cat_id}}" class="text-center">Clicks: <strong>{{cat_click_count}}</strong></h2>
+	<button class="clicker_button btn btn-primary center-block" id="cat_{{cat_id}}_button" data-cat_id={{cat_id}}>Vote for {{cat_name}}</button>
+`
 
+/// this will be generated
+var cats_list_html = "";
 
 $(document).ready(function() {
-	// generate cats html from datastructure
+	// generate list cats html from datastructure
 	for (var cat_id in cats) {
-		var cat_html_aux = cat_html;
+		var cat_list_html_aux = cat_list_html;
 		for (cat_attr in cats[cat_id]) {
-			if (cat_attr == "cat_image") {
-				cat_html_aux = cat_html_aux.replaceAll("{{" + cat_attr + "}}", config["images_dir"] + cats[cat_id][cat_attr]);	
-			} else {
-				cat_html_aux = cat_html_aux.replaceAll("{{" + cat_attr + "}}", cats[cat_id][cat_attr]);
-			}
-			
-			cat_html_aux = cat_html_aux.replaceAll("{{cat_id}}", cat_id);
+			cat_list_html_aux = cat_list_html_aux.replaceAll("{{cat_name}}", cats[cat_id]["cat_name"]);
+			cat_list_html_aux = cat_list_html_aux.replaceAll("{{cat_id}}", cat_id);
 		}
-		cats_html += cat_html_aux;
+		cats_list_html += cat_list_html_aux;
 	}
 
-	$("#cats-container").append(cats_html);
+	// when click on a cat link show the cat info on the pane
+	$("#cats-list").on('click', '.cat-link', function(e) {
+		var cat_id = $(this).data("cat_id");
+		var cat_html_aux = cat_html;
+		cat_html_aux = cat_html_aux.replaceAll("{{cat_id}}", cat_id);
+		cat_html_aux = cat_html_aux.replaceAll("{{cat_name}}", cats[cat_id]["cat_name"]);
+		cat_html_aux = cat_html_aux.replaceAll("{{cat_image}}", config["images_dir"] + cats[cat_id]["cat_image"]);
+		cat_html_aux = cat_html_aux.replaceAll("{{cat_click_count}}", cats[cat_id]["cat_click_count"]);
+		$("#cat-info").html(cat_html_aux);
+	});
 
-	$("#cats-container").on('click', '.clicker_button', function(e) {
+	$("#cats-list").append(cats_list_html);
+
+	// Vote on click button and update cont info
+	$("#cat-info").on('click', '.clicker_button', function(e) {
 		var cat_id = $(this).data("cat_id");
 		cats[cat_id]["cat_click_count"] = cats[cat_id]["cat_click_count"] + 1;
 		// update count view
-		$("#clicker_count_" + cat_id).text(cats[cat_id]["cat_click_count"]);
+		$("#clicker_count_" + cat_id).text("Clicks: " + cats[cat_id]["cat_click_count"]);
 		console.log(cats[cat_id]["cat_click_count"]);
 	});
 });
