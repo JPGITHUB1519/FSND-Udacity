53c7145aaa8bbbd3ba6531e2ce586cd01b6a2efc
Jean Urena <juanpedro1519@gmail.com>
2017-09-23 15:37:31 -0400

    [JS-DP] - Cat Clicker Premium Pro

--- Courses/JS_Design_Patterns/cat_clicker/assets/js/script.js
+++ Courses/JS_Design_Patterns/cat_clicker/assets/js/script.js
@@ -3,21 +3,26 @@ String.prototype.replaceAll = function(search, replacement) {
     return target.replace(new RegExp(search, 'g'), replacement);
 };
 
-var config = {
-	"images_dir": "assets/images/"
-}
-
-
 $(function() {
 	var model = {
 		currentCat: null,
-		cats: []
+		cats: [],
+		isAdmin: false
 	};
 
 	var octopus = {
-
+		/* Model Changing data methods */
 		addNewCat: function(cat_obj) {
 			model.add(cat_obj);
+		},
+
+		updadeCatInfo: function(cat_obj) {
+			model.currentCat.cat_name = cat_obj.cat_name;
+			model.currentCat.cat_image = cat_obj.cat_image;
+			model.currentCat.cat_click_count = cat_obj.cat_click_count;
+			viewCatList.render();
+			viewCatProfile.render();
+			viewCatAdmin.render();
 		},
 
 		fillModelWithDummyData: function() {
@@ -25,31 +30,31 @@ $(function() {
 				{
 					"cat_id": 1,
 					"cat_name": "Xuxa",
-					'cat_image': "xuxa_cat.jpg",
+					'cat_image': "assets/images/xuxa_cat.jpg",
 					"cat_click_count": 0
 				},
 				{
 					"cat_id": 2,
 					"cat_name": "Chewie",
-					'cat_image': "chewie_cat.jpg",
+					'cat_image': "assets/images/chewie_cat.jpg",
 					"cat_click_count": 0
 				},
 				{
 					"cat_id": 3,
 					"cat_name": "Jetske",
-					'cat_image': "jetske.jpg",
+					'cat_image': "assets/images/jetske.jpg",
 					"cat_click_count": 0
 				},
 				{
 					"cat_id": 4,
 					"cat_name": "Larry",
-					'cat_image': "larry.jpg",
+					'cat_image': "assets/images/larry.jpg",
 					"cat_click_count": 0
 				},
 				{
 					"cat_id": 5,
 					"cat_name": "Miso",
-					'cat_image': "miso.jpg",
+					'cat_image': "assets/images/miso.jpg",
 					"cat_click_count": 0
 				}
 			];
@@ -59,11 +64,11 @@ $(function() {
 		},
 
 		getCurrentCat: function() {
-			return model.current_cat;
+			return model.currentCat;
 		},
 
 		setCurrentCat: function(cat_obj) {
-			model.current_cat = cat_obj;
+			model.currentCat = cat_obj;
 		},
 
 		getCatById: function(cat_id) {
@@ -80,17 +85,15 @@ $(function() {
 			return model.cats[0];
 		},
 
-		voteCat: function(cat_id) {
+		/* View Methods */
+
+		voteCat: function() {
 			/**
-             * Vote for a cat and update the view
-             * 
-             */
-			model.cats.filter(function(cat_obj) {
-				if(cat_obj.cat_id == cat_id) {
-					cat_obj.cat_click_count += 1;
-				}
-			});
-			view_cat_profile.render();
+			 * Vote for a cat and update the view
+			 * 
+			 */
+			 model.currentCat["cat_click_count"] = parseInt(model.currentCat["cat_click_count"]) + 1;
+			 viewCatProfile.render();
 		},
 
 		updateCatProfile: function(cat_id) {
@@ -100,18 +103,36 @@ $(function() {
              */
 			var cat_obj = this.getCatById(cat_id);
 			octopus.setCurrentCat(cat_obj);
-			view_cat_profile.render();
+			viewCatProfile.render();
+			viewCatAdmin.render();
 		},	
+
+		setAdminFalse: function() {
+			model.isAdmin = false;
+		},
+
+		activeAdmin: function() {
+			model.isAdmin = true;
+			viewCatAdmin.render();
+			viewCatAdmin.adminForm.show()
+		},
+
+		disableAdmin: function() {
+			model.isAdmin = false;
+			viewCatAdmin.render();
+			viewCatAdmin.adminForm.hide();
+		},
 
 		init: function() {
 			this.fillModelWithDummyData();
 			this.setCurrentCat(model.cats[0]);
-			view_cats_list.init();
-			view_cat_profile.init();
+			viewCatList.init();
+			viewCatProfile.init();
+			viewCatAdmin.init();
 		}
 	}
 
-	var view_cats_list = {
+	var viewCatList = {
 		init: function() {
 			// saving the view elements on the view to performance 
 			this.catsList = $("#cats-list");
@@ -128,6 +149,7 @@ $(function() {
 
 		render: function() {
 			/* Render list of cats */
+			this.catsList.html("");
 			model.cats.forEach(function(cat_obj, index) {
 				var this_template = this.cat_list_template.replaceAll("{{cat_name}}", cat_obj.cat_name);
 				this_template = this_template.replaceAll("{{cat_id}}", cat_obj.cat_id);
@@ -136,7 +158,7 @@ $(function() {
 		}
 	}
 
-	var view_cat_profile = {
+	var viewCatProfile = {
 		init: function() {
 			this.cat_info = $("#cat-info");
 			this.cat_info_template = $('script[data-template="cat-info"]').html();
@@ -144,19 +166,59 @@ $(function() {
 
 			// Vote on click button and update cont info
 			$("#cat-info").on('click', '.clicker_button', function(e) {
-				var cat_id = $(this).data("cat_id");
-				octopus.voteCat(cat_id);
+				octopus.voteCat();
 			});
 
 		},
 
 		render: function() {
-			var current_cat = octopus.getCurrentCat();
-			var this_template = this.cat_info_template.replaceAll("{{cat_id}}", current_cat.cat_id);
-			this_template = this_template = this_template.replaceAll("{{cat_name}}", current_cat.cat_name);
-			this_template = this_template.replaceAll("{{cat_image}}", config["images_dir"] + current_cat.cat_image);
-			this_template = this_template.replaceAll("{{cat_click_count}}", current_cat.cat_click_count);
+			var currentCat = octopus.getCurrentCat();
+			var this_template = this.cat_info_template.replaceAll("{{cat_id}}", currentCat.cat_id);
+			this_template = this_template = this_template.replaceAll("{{cat_name}}", currentCat.cat_name);
+			this_template = this_template.replaceAll("{{cat_image}}", currentCat.cat_image);
+			this_template = this_template.replaceAll("{{cat_click_count}}", currentCat.cat_click_count);
 			this.cat_info.html(this_template);
+		}
+	}
+
+	var viewCatAdmin = {
+		init: function() {
+			this.adminForm = $("#admin-form");
+			this.adminSaveButton = $("#admin-save-button");
+			this.adminCancelButton = $("#admin-cancel-button");
+			this.adminTriggerButton = $("#admin-trigger-button");
+			this.catNameInput = $("#cat-name-input");
+			this.catImageInput = $("#cat-image-input");
+			this.catVotesInput = $("#cat-votes-input");
+			
+			// events listeners
+			this.adminTriggerButton.click(function(e) {
+				octopus.activeAdmin();
+			});
+
+			this.adminSaveButton.click(function(e) {
+				// prevent trigger button for get
+				e.preventDefault();
+				octopus.updadeCatInfo({
+					cat_name: this.catNameInput.val(),
+					cat_image: this.catImageInput.val(),
+					cat_click_count: this.catVotesInput.val()
+				});
+			}.bind(this));
+
+			this.adminCancelButton.click(function(e) {
+				e.preventDefault();
+				octopus.disableAdmin();
+			});
+
+			this.adminForm.hide();
+		},
+
+		render: function() {
+			var currentCat = octopus.getCurrentCat();
+			this.catNameInput.val(currentCat.cat_name);
+			this.catImageInput.val(currentCat.cat_image)
+			this.catVotesInput.val(currentCat.cat_click_count);
 		}
 	}
 
